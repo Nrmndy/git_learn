1b94d8af4c19ecc9909389502ea73aa1ee1d54ed
Andrew Karpov <aggressivity@gmail.com>
2022-06-24 10:29:12 +0300

    added command_1 (say_hello)

---
+++ src/SayHello.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class SayHello extends Command
+{
+    protected static $defaultName = 'say_hello';
+	
+	protected function configure(): void
+    {
+        $this
+            ->setHelp('This command says Hello to your Input')
+			->addArgument('userInput', InputArgument::REQUIRED, 'The Input to say hello to.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('Привет '.$input->getArgument('userInput'));
+        return Command::SUCCESS;
+    }
+}
