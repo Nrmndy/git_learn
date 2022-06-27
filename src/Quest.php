830aa6d9047018af14652629519d0708b93d5c3c
Andrew Karpov <aggressivity@gmail.com>
2022-06-27 17:14:08 +0300

    fixed code formatting

--- src/Quest.php
+++ src/Quest.php
@@ -13,37 +13,35 @@ use Symfony\Component\Console\Question\ChoiceQuestion;
 class Quest extends Command
 {
     protected static $defaultName = 'quest';
-	
-	protected function configure(): void
+
+    protected function configure(): void
     {
-        $this
-            ->setHelp('This command collects info about you and outputs it in console.')
-        ;
+        $this->setHelp('This command collects info about you and outputs it in console.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $helper = $this->getHelper('question');
-        
+
         $outputStyleGreen = new OutputFormatterStyle('green');
         $outputStyleMagenta = new OutputFormatterStyle('magenta');
-        
+
         $output->getFormatter()->setStyle('green', $outputStyleGreen);
         $output->getFormatter()->setStyle('pink', $outputStyleMagenta);
-        
-		$question = new Question('Введите Ваше имя: ', 'Не указано');
+
+        $question = new Question('Введите Ваше имя: ', 'Не указано');
         $name = $helper->ask($input, $output, $question);
-        
+
         $question = new Question('Введите Ваш возраст: ', 'Не указан');
         $age = $helper->ask($input, $output, $question);
-        
+
         $question = new ChoiceQuestion(
-        'Укажите Ваш пол: ',
-        ['М', 'Ж'],
-        0
+            'Укажите Ваш пол: ',
+            ['М', 'Ж'],
+            0
         );
         $gender = $helper->ask($input, $output, $question);
-                
+
         $output->writeln('Вас зовут: <green>' . $name . '</>');
         $output->writeln('Ваш возраст: <green>' . $age . '</>');
         $output->writeln('Ваш пол:' . (($gender === 'Ж') ? '<pink> ' : '<green> ') . $gender . '</>');
