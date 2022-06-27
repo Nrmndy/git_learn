3346ca9a8743a97c3936ea5d25b272115cd19795
Andrew Karpov <aggressivity@gmail.com>
2022-06-27 17:04:40 +0300

    fixed code formatting

--- src/Repeater.php
+++ src/Repeater.php
@@ -11,23 +11,23 @@ use Symfony\Component\Console\Input\InputArgument;
 class Repeater extends Command
 {
     protected static $defaultName = 'repeat';
-	
-	protected function configure(): void
+
+    protected function configure(): void
     {
         $this
             ->setHelp('This command repeats your Input several times')
-		    ->addArgument('userInput', InputArgument::REQUIRED, 'The Input to repeat.')
-		    ->addArgument('counter', InputArgument::OPTIONAL, 'How many times you want to repeat your phrase?')
-        ;
+            ->addArgument('userInput', InputArgument::REQUIRED, 'The Input to repeat.')
+            ->addArgument('counter', InputArgument::OPTIONAL, 'How many times you want to repeat your phrase?');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-		$counter = $input->getArgument('counter') ?? 2;
-		
-		for ($i = 0; $i < $counter; $i++) {
-		    $output->writeln($input->getArgument('userInput'));
-		}
+        $counter = $input->getArgument('counter') ?? 2;
+
+        for ($i = 0; $i < $counter; $i++) {
+            $output->writeln($input->getArgument('userInput'));
+        }
+
         return Command::SUCCESS;
     }
 }
